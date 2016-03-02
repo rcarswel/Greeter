49c639b543828bd32729baee37ec9ffbfee9aa89
rcarswel <rcarswel@westga.edu>
2016-03-01 20:21:00 -0500

    Extends class for testing Android activity.

---
+++ app/src/androidTest/java/edu/westga/cs6242/greeter/MainActivityTests.java
@@ -0,0 +1,15 @@
+package edu.westga.cs6242.greeter;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MainActivityTests extends
+        ActivityInstrumentationTestCase2<MainActivity> {
+    public MainActivityTests() {
+        super(MainActivity.class);
+    }
+
+    public void testActivityExists() {
+        MainActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
